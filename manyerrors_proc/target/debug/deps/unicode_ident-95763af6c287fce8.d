bf8daad400217643d95ac23c5259b91a56ae6681
Will <willsorenson@gmail.com>
2026-07-26 16:57:35 -0600

    fixed manyerrors_proc for uploading

--- manyerrors_proc/target/debug/deps/unicode_ident-95763af6c287fce8.d
+++
@@ -1,8 +0,0 @@
-C:\dev\rust\manyerrors\manyerrors_proc\target\debug\deps\unicode_ident-95763af6c287fce8.d: C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\dev\rust\manyerrors\manyerrors_proc\target\debug\deps\libunicode_ident-95763af6c287fce8.rlib: C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\dev\rust\manyerrors\manyerrors_proc\target\debug\deps\libunicode_ident-95763af6c287fce8.rmeta: C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\Users\wills\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
--- manyerrors_proc/target/debug/manyerrors_proc.d
+++
@@ -1,1 +0,0 @@
-C:\dev\rust\manyerrors\manyerrors_proc\target\debug\manyerrors_proc.dll: C:\dev\rust\manyerrors\manyerrors_proc\target\package\manyerrors_proc-1.0.0\src\lib.rs
